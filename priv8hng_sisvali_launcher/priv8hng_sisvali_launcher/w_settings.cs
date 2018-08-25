cd818d8d35946c3bb33d42f3e40a26a732fba121
lavaphox <l4v4ph0x@riseup.net>
2018-08-25 14:14:17 -0700

    formatted

--- priv8hng_sisvali_launcher/priv8hng_sisvali_launcher/w_settings.cs
+++ priv8hng_sisvali_launcher/priv8hng_sisvali_launcher/w_settings.cs
@@ -8,37 +8,37 @@ using System.Text;
 using System.Windows.Forms;
 
 namespace priv8hng_sisvali_launcher {
-	public partial class w_settings : Form {
-		public w_settings() {
-			InitializeComponent();
-		}
+  public partial class w_settings : Form {
+    public w_settings() {
+      InitializeComponent();
+    }
 
-		private void btn_cancel_Click(object sender, EventArgs e) {
-			((w_main)this.w_panel.Parent.Parent).btn_settings_cancel();
-		}
+    private void btn_cancel_Click(object sender, EventArgs e) {
+      ((w_main)this.w_panel.Parent.Parent).btn_settings_cancel();
+    }
 
-		private string lastIp = "255.255.255.255";
-		private void tbx_ip_TextChanged(object sender, EventArgs e) {
-			IPAddress address;
-			if (IPAddress.TryParse(tbx_ip.Text, out address)) {
-				lastIp = tbx_ip.Text;
-			} else {
-				int select = tbx_ip.SelectionStart;
-				tbx_ip.Text = lastIp;
-				tbx_ip.Select(select -1, select -2);
-			}
-		}
+    private string lastIp = "255.255.255.255";
+    private void tbx_ip_TextChanged(object sender, EventArgs e) {
+      IPAddress address;
+      if (IPAddress.TryParse(tbx_ip.Text, out address)) {
+        lastIp = tbx_ip.Text;
+      } else {
+        int select = tbx_ip.SelectionStart;
+        tbx_ip.Text = lastIp;
+        tbx_ip.Select(select - 1, select - 2);
+      }
+    }
 
-		private string lastPort = "65535";
-		private void tbx_port_TextChanged(object sender, EventArgs e) {
-			ushort port;
-			if (ushort.TryParse(tbx_port.Text, out port)) {
-				lastPort = tbx_port.Text;
-			} else {
-				int select = tbx_port.SelectionStart;
-				tbx_port.Text = lastPort;
-				tbx_port.Select(select -1, select -2);
-			}
-		}
-	}
+    private string lastPort = "65535";
+    private void tbx_port_TextChanged(object sender, EventArgs e) {
+      ushort port;
+      if (ushort.TryParse(tbx_port.Text, out port)) {
+        lastPort = tbx_port.Text;
+      } else {
+        int select = tbx_port.SelectionStart;
+        tbx_port.Text = lastPort;
+        tbx_port.Select(select - 1, select - 2);
+      }
+    }
+  }
 }
